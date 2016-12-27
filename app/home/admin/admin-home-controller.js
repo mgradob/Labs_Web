28e45664f9efb3319d3183bcb64a31a710f74962
Miguel Grado <mgradob@gmail.com>
2016-12-27 15:24:49 -0700

    Adding home

--- app/home/admin/admin-home-controller.js
+++ app/home/admin/admin-home-controller.js
@@ -2,6 +2,42 @@
  * Created by mgradob on 11/25/16.
  */
 angular.module('labs-cuu')
-    .controller('AdminHomeController', function ($scope, $state, $stateParams) {
+    .controller('AdminHomeController', function ($scope, $state, $stateParams, $localStorage) {
+        $scope.userId = $localStorage.user.id_user;
 
+        $localStorage.lab = $stateParams.lab;
+
+        $scope.goToHome = function () {
+            $state.go('adminHome.dashboard');
+        };
+
+        $scope.goToRequests = function () {
+            // todo
+        };
+
+        $scope.goToNewUsers = function () {
+            // todo
+        };
+
+        $scope.goToInventory = function () {
+            $state.go('adminHome.categories');
+        };
+
+        $scope.goToUsers = function () {
+            // todo
+        };
+
+        $scope.goToLabsList = function () {
+            // todo
+        };
+
+        $scope.goToConfig = function () {
+            // todo
+        };
+
+        $scope.signOut = function () {
+            // todo
+        };
+
+        $scope.goToHome();
     });
--- app/home/home-service.js
+++ app/home/home-service.js
@@ -2,6 +2,50 @@
  * Created by mgradob on 11/25/16.
  */
 angular.module('labs-cuu')
-    .service('HomeService', function ($http) {
+    .service('HomeService', function ($http, $localStorage) {
+        this.getUserHome = function () {
+            var url = BASE_URL + '/home/' + $localStorage.user.id_user;
 
+            return $http.get(url, {
+                    headers: {
+                        "Authorization": $localStorage.token
+                    },
+                    params: {
+                        lab_id: $localStorage.lab.id
+                    }
+                })
+                .then(function (response) {
+                    return response.data;
+                })
+        };
+
+        this.getLabInventory = function () {
+            var url = BASE_URL + '/inventory/' + $localStorage.lab.id;
+
+            return $http.get(url, {
+                    headers: {
+                        "Authorization": $localStorage.token
+                    },
+                    params: {
+                        id_user: $localStorage.user.id_user
+                    }
+                })
+                .then(function (response) {
+                    return response.data;
+                })
+        };
+
+        this.postCategory = function (newCategory) {
+            var url = BASE_URL + '/inventory/' + $localStorage.lab.id;
+
+            var body = {
+                name: newCategory.name,
+                components: newCategory.components
+            };
+
+            return $http.post(url, body)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
     });
