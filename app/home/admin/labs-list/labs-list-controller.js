eebd124f13805b5ffcee6d3b9bf98a79fd72bd8c
Miguel Grado <mgradob@gmail.com>
2016-12-27 15:26:15 -0700

    Adding labs list

---
+++ app/home/admin/labs-list/labs-list-controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mgradob on 12/16/16.
+ */
+angular.module('labs-cuu')
+    .controller('LabsListController', function ($scope, $state, $stateParams, $localStorage) {
+        $scope.labs = $stateParams.user.labs;
+
+        $scope.goToLab = function (lab) {
+            $localStorage.lab = lab;
+
+            $state.go('adminHome');
+        }
+    });
